33e1989a5c0bc345c00c05c6873742159173479a
irisotano2 <irisotano@gmail.com>
2024-06-06 22:55:12 +0000

    First visualization

---
+++ notebooks/10_Streamlit/apps/app.py
@@ -0,0 +1,19 @@
+# // $ streamlit run app.py to run it on the browser
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv ('../../../data/Datafiniti_Fast_Food_Restaurants_Jun19.csv')
+dfres = df[['name', 'province']].value_counts().reset_index().head(100)
+
+
+st.title('Fast Food Count Across the US')
+st.write ('Built by [Iris Otano](https://www.linkedin.com/in/irisotano/)')
+fig = px.bar(
+    data_frame=dfres, x='province', y='count',color='name', facet_col='name', facet_col_wrap=3, height=1000)
+
+
+st.plotly_chart(fig)
+
+
+dfres
